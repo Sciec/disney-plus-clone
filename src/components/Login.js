ba4564f4a148b736079e504b7499d0c1cc27999d
Sciec <rahulsaxena2021@hotmail.com>
2023-05-12 03:23:25 +0530

    add the rest of the hero element of the starting page

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,9 +6,18 @@ const Login = (props) => {
             <Content>
                 <CTA>
                     <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+                    <Signup>GET ALL THERE</Signup>
+                    <Description>
+                        Get Premier Access to Raya and the Last Dragon for an 
+                        additional feewith a Disney+ subscription. As of 03/26/21,
+                        the price of Disney+ and The Disney Bundle will increase 
+                        by $1.
+                    </Description>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+                    
                 </CTA>
-
-
+                
+                
                 <BgImage />
             </Content>
         </Container>
@@ -87,12 +96,12 @@ const CTA = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
-
+    text-align: center;
+    
     /* flex-wrap: wrap;
+    align-items: center;
     justify-content: center;
     margin-top: 0;
-    align-items: center;
-    text-align: center;
     margin-right: auto;
     margin-left: auto;
     transition-timing-function: ease-out;
@@ -105,6 +114,43 @@ const CTALogoOne = styled.img`
     max-width: 600px;
     min-height: 1px;
     display: block;
+    width: 100%;
+`
+
+const Signup = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    /* text-align: center; */
+
+    &:hover {
+        background-color: #0483ee;
+        
+    }
+`
+
+const Description = styled.p`
+    /* text-align: center; */
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
+
+`;
+
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
     width: 100%;
 `
 
