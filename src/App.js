167a4b9ef32430b0e62f1dbc3522cb4501689e85
Sciec <rahulsaxena2021@hotmail.com>
2023-05-12 00:14:51 +0530

    importing components from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World!</h2>
+      <Router>
+        <Switch>
+            <Route exact path='/' >
+              
+            </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
