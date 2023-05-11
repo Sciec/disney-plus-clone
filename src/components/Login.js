5f73689ddeecc2e8bda8b10c2038814c632b90c9
Sciec <rahulsaxena2021@hotmail.com>
2023-05-12 01:08:29 +0530

    Render the Content of Login to the Route using styled-components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,32 @@
-import { Styled } from "styled-components";
-import { Container } from "reactstrap";
+import { Styled, styled } from "styled-components";
 
 const Login = (props) => {
-    <Container>
+    return (
+        <Container>
+            <Content>Content</Content>
+        </Container>
+    );
+};
 
-    </Container>
-}
+
+/*=========================================================
+=            using styled-compenents for Login            =
+=========================================================*/
+
+
+/* Every style related to Login component can be styled inside the js file only using styled- components */
+
+const Container = styled.section`
+ overflow: hidden;
+ display: flex;
+`;
+
+const Content = styled.div``;
+
+
+/*=====  End of using styled-compenents for Login  ======*/
+
+
+
 
 export default Login;
