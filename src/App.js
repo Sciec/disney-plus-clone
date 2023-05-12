ff046ab48c264372f633b5f70eae2969d1004f0b
Sciec <rahulsaxena2021@hotmail.com>
2023-05-12 19:19:19 +0530

    Route the Home components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Header.js';
 import Login from './components/Login.js';
+import Home from './components/Home.js';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router path='/auth'>
-          <Header />
-          <Login />
-      </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+          <Route exact path='/home' element={<Home />} />
+              
+        </Routes> 
+      </Router> 
+
     </div>
   );
 }
