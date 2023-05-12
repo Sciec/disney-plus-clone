12532a1392ea932e7974f22878bdd56740d28412
Sciec <rahulsaxena2021@hotmail.com>
2023-05-13 02:34:18 +0530

    change Slider to Carousel and add images to slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -15,18 +15,102 @@ const ImgSlider = (props) => {
     };
 
     return (
-            <Slider {...settings}>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-            </Slider>
+            <Carousel {...settings}>
+                
+                <Wrap>
+                    <a>
+                        <img src='/images/slider-badging.jpg' alt="" />
+                    </a>
+                </Wrap>
+                
+                <Wrap>
+                    <a>
+                        <img src='/images/slider-scale.jpg' alt="" />
+                    </a>
+                </Wrap>
+                
+                <Wrap>
+                    <a>
+                        <img src='/images/slider-badag.jpg' alt="" />
+                    </a>
+                </Wrap>
+                
+                <Wrap>
+                    <a>
+                        <img src='/images/slider-scales.jpg' alt="" />
+                    </a>
+                </Wrap>
+            </Carousel>
     );
 };
+
+const Carousel = styled(Slider)`
+    margin-top: 20px;
+    & > button {
+        opacity: 0;
+        height: 100%;
+        width: 5vw;
+        z-index: 1;
+
+        &:hover {
+            opacity: 1;
+            transition: opacity 0.2s ease-in 0s;
+        }
+    }
+
+    ul li button {
+        &:before {
+            font-size: 10px;
+            color: rgb(150, 158, 171);
+
+        }
+    }
+
+    li.slick-active button:before {
+        color: white;
+
+    }
+
+    .slick-list {
+        overflow: initial;
+    }
+
+    .slick-prev {
+        left: -75px;
+
+    }
+
+    .slick-next {
+        right: -75px;
+    }
+`;
+
+const Wrap = styled.div`
+    border-radius: 4px;
+    cursor: pointer;
+    position: relative;
+
+    a {
+        border-radius: 4px;
+        box-shadow: rgb(0 0 0 / 69%) 0 26px 30px, rgb(0, 0, 0 / 73%) 0 16px 10px;
+        cursor: pointer;
+        display: block;
+        position: relative;
+        padding: 4px;
+
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 4px;
+        }
+
+        &:hover {
+            padding: 0;
+            border: 4px solid rgba(249, 249, 249, 0.8);
+            transition-duration: 300ms;
+            
+        }
+    }
+`;
 
 export default ImgSlider;
