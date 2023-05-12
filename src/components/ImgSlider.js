42fc60d8188c2753d7bc2d352373528ad1552a26
Sciec <rahulsaxena2021@hotmail.com>
2023-05-13 02:03:16 +0530

    add the Slider tag from the module and see the changes

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,9 +1,32 @@
 import { styled } from "styled-components";
 
-const ImgSlider = (props => {
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick";
+
+const ImgSlider = (props) => {
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+    };
+
     return (
-        <h1>Image Slider</h1>
-    )
-})
+            <Slider {...settings}>
+                <div>
+                    <h3>1</h3>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+            </Slider>
+    );
+};
 
 export default ImgSlider;
