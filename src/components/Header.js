004cc6f7bbb749863c222fbe20c7a4b35cba672b
Sciec <rahulsaxena2021@hotmail.com>
2023-05-12 05:33:08 +0530

    add login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,6 +32,8 @@ const Header = (props) => {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
+
+            <Login>Login</Login>
         </Nav>
     )
 }
@@ -149,6 +151,23 @@ const NavMenu = styled.div`
 
     @media (max-width: 768px) {
     display: none;
+    }
+`;
+
+const Login = styled.a`
+    font-weight: 550;
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all 0.2s ease-out 0s;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: black;
+        border-color: transparent;
     }
 `;
 
